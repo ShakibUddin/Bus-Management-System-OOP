a5d3f8d7c15443e25dc501f0c1b554687c7d5484
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2026-05-26 21:32:33 +0600

    First commit, added home screen

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("              Bus Management System          ");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("1. Create User");
+            Console.WriteLine("2. Show All Users");
+            Console.WriteLine("3. Create Bus");
+            Console.WriteLine("4. Show All Bus");
+            Console.WriteLine("5. Create Schedule");
+            Console.WriteLine("6. Show All Schedule");
+            Console.WriteLine("7. Show A Schedule Details");
+            Console.WriteLine("8. Book Ticket");
+            Console.WriteLine("9. Show User Tickets");
+            Console.WriteLine("10. Show User Invoices");
+            Console.WriteLine("11. Make Payment");
+            Console.WriteLine("12. Exit");
+
+            Console.WriteLine("");
+            Console.WriteLine("==============================================");
+
+            string input = Console.ReadLine();
+
+            if (input == "12")
+                break;
+
+            Console.WriteLine("You typed: " + input);
+        }
+    }
+}
